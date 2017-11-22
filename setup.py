b70cc59a39db0d4d6fe74c68282b688fd39f1804
ran@panorays.com <ran@panorays.com>
2017-11-22 13:44:16 +0200

    Bump version: 61.0.0 → 62.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='git_releaser',
-    version='61.0.0',
+    version='62.0.0',
     description='A quick script to commit, handle versionning and generate changelogs',
     long_description=open('README.rst').read(),
     author='Ran Shamay',
