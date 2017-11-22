b824a0d640243c71c10efc8efd123af10c055ada
ran@panorays.com <ran@panorays.com>
2017-11-22 17:03:10 +0200

    added tags support

--- git_releaser/__main__.py
+++ git_releaser/__main__.py
@@ -4,17 +4,16 @@ Bumpversion and generate a changelog straight from your git commits.
 Usage: git-releaser [options]
 
 options:
-    -r --repo                           Path to the repository's root directory
-    -t --template-dir                   The directory containing the templates used for
-                                        rendering the changelog
-    -h --help                           Print this help text
-    -p --promote [default:patch]        Major,minor or patch version [Default: patch]
-    -V --version                        Print the version number
+    -r --repo [default:os.getcwd()]        Path to the repository's root directory
+    -h --help                              Print this help text
+    -p --promote [default:patch]           Major,minor or patch version [Default: patch]
+    -V --version                           Print the version number
 """
-import bumpversion
-import docopt
 import os
-import sys
+
+import docopt
+
+import bumpversion
 from git_releaser import __version__
 
 CONFIG_FILE_LOCATION = os.path.join(os.getcwd(), 'setup.cfg')
@@ -22,15 +21,17 @@ CONFIG_FILE_LOCATION = os.path.join(os.getcwd(), 'setup.cfg')
 
 def main():
     args = docopt.docopt(__doc__, version=__version__)
-    # cz(["commit"])
     changelog_args = list()
     changelog_args.append('--repo')
-    if '--repo' not in sys.argv:
-        changelog_args.append(os.getcwd())
-    else:
-        changelog_args.append(args.get('--repo'))
+    base_name = os.getcwd()
+    if '--repo' in args and args['--repo'] is not None:
+        base_name = args.get('--repo')
+    changelog_args.append(base_name)
+    repo_name = os.path.basename(base_name)
+    changelog_args.append('--repo-name')
+    changelog_args.append(repo_name)
     changelog_args.append("--template-dir")
-    changelog_args.append("/Users/ran_shamay/devl/Code/auto-changelog/auto_changelog/templates")
+    changelog_args.append('/tmp/templates')
     promote_kinds = ['patch', 'major', 'minor']
     next_version_bump_policy = 'patch'
     if args.get('--promote') in promote_kinds:
@@ -38,7 +39,7 @@ def main():
     bumpversion.main(
         ['--allow-dirty', '--config-file', CONFIG_FILE_LOCATION, next_version_bump_policy, "--tag", "--commit"],
         changelog_args)
-    print """type "git push" to push all changes to git"""
+    print """type `git push --tags origin master` to push all changes to git"""
 
 
 if __name__ == "__main__":
